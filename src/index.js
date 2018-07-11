1a376e6cc1f80e4667015540afc90b252cddefb8
Mary <mumoh@mozilla.com>
2018-07-10 22:29:27 -0400

    Edit index files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/common/Header';
+
+const App = () => {
+  return ( 
+    <div>
+    <Header /> 
+    <h1>Coinerstone</h1>
+    <p>Get the latest crypto currency data!</p>
+    </div>
+  )
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
