46658ac22bd73e85445f3f51d7727567bedcbcfb
Mary <mumoh@mozilla.com>
2018-07-14 15:57:50 -0400

    Add React Router, detail component

--- src/components/list/Table.js
+++ src/components/list/Table.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Table.css';
 
 const Table = (props) => {
-  const { currencies, renderPercentChange } = props;
+  const { currencies, renderPercentChange, history } = props;
 
   return (
        <div className="Table-container">
@@ -18,7 +19,10 @@ const Table = (props) => {
           </thead>
           <tbody className="Table-body">
           {currencies.map((currency) => (
-            <tr key={currency.id}>
+            <tr 
+              key={currency.id}
+              onClick={() => history.push(`/currency/${currency.id}`)}
+            >
               <td>
                 <span className="Table-rank">{currency.rank}</span>
                 {currency.name}
@@ -45,6 +49,7 @@ const Table = (props) => {
 Table.PropTypes = {
   currencies: PropTypes.array.isRequired,
   renderPercentChange: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
-export default Table;
+export default withRouter(Table);
