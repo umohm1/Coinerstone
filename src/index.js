3153c088d4cb927765baa742b39b045d0d904511
Mary <mumoh@mozilla.com>
2018-07-14 10:43:54 -0400

    Add list component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
+import List from './components/list/List';
 import './index.css';
 
 const App = () => {
   return ( 
     <div>
     <Header /> 
-    <h1>Coinerstone</h1>
-    <p>Get the latest crypto currency data!</p>
+    <List />
     </div>
   )
 };
