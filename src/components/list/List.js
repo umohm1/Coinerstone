0107e0361d446e89ce3d6bb49cd6b44083ccee22
Mary <mumoh@mozilla.com>
2018-07-14 11:05:06 -0400

    Continue building List.js

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
+
+
+
 class List extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      currencies: [],
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20')
+      .then(response => {
+        return response.json().then(json => {
+          return response.ok ? json : Promise.reject(json);
+        });
+      }) 
+      .then((data) => { 
+        console.log('Success', data);
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  }
+
+
   render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>
+    }
+
     return (
       <div>text</div>
     );
