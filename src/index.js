e40b0671998db92b3b1cd2fd9103b6c7623d1441
Mary <mumoh@mozilla.com>
2018-07-14 14:34:17 -0400

    Add NotFound component and corresponding CSS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
+import NotFound from './components/notfound/NotFound';
 import './index.css';
 
 const App = () => {
   return ( 
+    <BrowserRouter>
     <div>
-    <Header /> 
-    <List />
+      <Header /> 
+
+      <Switch>
+        <Route path="/" component={List} exact />
+        <Route component={NotFound} />
+      </Switch>
     </div>
-  )
-};
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.render(
   <App />,
